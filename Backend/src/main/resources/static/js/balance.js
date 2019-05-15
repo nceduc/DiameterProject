341cbec33e0b04ad0b66ee71ec2cd2ecd1cb7ef1
vikzelikov <vikzelikov@yandex.ru>
2019-05-15 18:02:08 +0300

    ToFixed digit balance

--- Backend/src/main/resources/static/js/balance.js
+++ Backend/src/main/resources/static/js/balance.js
@@ -1,9 +1,16 @@
+var response;
 function getBalance(){
     $.get({
         url: "/dashboard/getbal",
         dataType:"json",
         success: function (result) {
-            $("#resultBal").html(result["response"]);
+            response = +result["response"];
+            if(!isNaN(response)){
+                response = response.toFixed(2);
+            }else{
+                response = result["response"];
+            }
+            $("#resultBal").html(response);
             $('#resultTime').html(result["date"]);
         }
     });
